f3182687c41e7c7f7420fdb4edb37cf98fff4094
benofben <ben.lackey@outlook.com>
2016-08-17 15:54:37 -0700

    to latest zip and drop unused

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
 cloud_type="aws"
-location=$1 #this is the location of the seed, not necessarily of this node
-unique_string=$2
-data_center_name=$3
-
-seed_node_dns_name="dc0vm0$unique_string.$location.cloudapp.azure.com"
+seed_node_dns_name="dc0vm0"
+data_center_name=$1
 
 echo "Configuring nodes with the settings:"
 echo cloud_type $cloud_type
-echo location $location
-echo unique_string $unique_string
-echo data_center_name $data_center_name
 echo seed_node_dns_name $seed_node_dns_name
+echo data_center_name $data_center_name
 
-wget https://github.com/DSPN/install-datastax/archive/1.0.zip
 apt-get -y install unzip
-unzip 1.0.zip
-cd install-datastax-1.0/bin
+
+wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-4.zip
+unzip 5.0.1-4.zip
+cd install-datastax-ubuntu-5.0.1-4/bin
 
 ./dse.sh $cloud_type $seed_node_dns_name $data_center_name
