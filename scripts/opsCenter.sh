7ff8102952bfabbf5e8faec0a52d4180a00fb848
Collin Poczatek <collin.poczatek@gmail.com>
2016-09-21 15:09:28 -0700

    add pull from head of ubuntu repo, commented out right now

--- scripts/opsCenter.sh
+++ scripts/opsCenter.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cloud_type="aws"
+cloud_type="ec2"
 seed_node_dns_name="dc0vm0"
 
 echo "Configuring nodes with the settings:"
@@ -12,5 +12,11 @@ apt-get -y install unzip
 wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-4.zip
 unzip 5.0.1-4.zip
 cd install-datastax-ubuntu-5.0.1-4/bin
+
+# Let's try from the repo head...
+#sudo apt-get -y install unzip && \
+#wget https://github.com/DSPN/install-datastax-ubuntu/archive/master.zip && \
+#unzip master.zip && \
+#cd install-datastax-ubuntu-master/bin/
 
 ./opscenter.sh $cloud_type $seed_node_dns_name
