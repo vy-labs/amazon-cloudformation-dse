8e4f52fea28b0262b886c16310f6e85f083d6673
Collin Poczatek <collin.poczatek@gmail.com>
2016-09-29 13:23:15 -0700

    clean up params

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Usage deploy.sh stack-name key-pair
+
 aws cloudformation validate-template \
 --template-body "$(cat ./cloudformation_dse_with_autoscale.json)"
 
@@ -7,6 +9,5 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --template-body "$(cat ./cloudformation_dse_with_autoscale.json)" \
 --parameters \
-ParameterKey=KeyName,ParameterValue="jcp-keypair" \
-ParameterKey=OperatorEMail,ParameterValue="collin.poczatek@datastax.com" \
-ParameterKey=VpcId,ParameterValue="jcp-vpc"
+ParameterKey=KeyName,ParameterValue=$2 \
+ParameterKey=OperatorEMail,ParameterValue="donotreply@datastax.com"
