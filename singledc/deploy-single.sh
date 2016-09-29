256918898d2b273529938027ea903cbb387214bc
Collin Poczatek <collin.poczatek@gmail.com>
2016-09-29 13:25:54 -0700

    cleanup names

--- singledc/deploy-single.sh
+++ singledc/deploy-single.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Usage: deploy-single.sh stack-name key-pair
+# Usage deploy.sh stack-name key-pair
 
 aws cloudformation validate-template \
---template-body "$(cat ./onemachine.json)"
+--template-body "$(cat ./single-node.json)"
 
 aws cloudformation create-stack \
 --stack-name $1 \
---template-body "$(cat ./onemachine.json)" \
+--template-body "$(cat ./single-node.json)" \
 --parameters \
-ParameterKey=KeyName,ParameterValue=$2
+ParameterKey=KeyName,ParameterValue=$2 \
+ParameterKey=OperatorEMail,ParameterValue="donotreply@datastax.com"
