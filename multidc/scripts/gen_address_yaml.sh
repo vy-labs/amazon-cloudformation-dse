3f9f82abcbd413ae1df30194a29a333e725076f1
Collin Poczatek <collin.poczatek@gmail.com>
2016-10-13 12:55:33 -0700

    silence curl

--- multidc/scripts/gen_address_yaml.sh
+++ multidc/scripts/gen_address_yaml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-privateip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-publicip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+privateip=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+publicip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 
 echo -e "agent_rpc_interface: $privateip
 agent_rpc_broadcast_address: $publicip
