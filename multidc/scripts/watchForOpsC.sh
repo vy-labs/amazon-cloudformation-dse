832e3b59050051e29a45c95766e05df36cfbfd93
Collin Poczatek <collin.poczatek@gmail.com>
2016-10-26 15:53:31 -0700

    typo

--- multidc/scripts/watchForOpsC.sh
+++ multidc/scripts/watchForOpsC.sh
@@ -5,5 +5,5 @@ OPSCIP='107.23.208.9'
 while true; do
   sleep 5;
   echo "curl opsc -----------------------------";
-  curl -s -I -m 2 http://$OPSCIP:8888/api/v1/lcm/ 2>&1 | tee watchforlcm.txt;
+  curl -s -I -m 2 http://$OPSCIP:8888/api/v1/lcm/ 2>&1 | tee watchforopsc.txt;
 done
