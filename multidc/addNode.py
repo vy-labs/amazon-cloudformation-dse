a3c5aaddc4655a4fe636414c0ae7076093e28f43
Collin Poczatek <collin.poczatek@gmail.com>
2016-10-28 19:32:56 -0700

    commit early, commit often

--- multidc/addNode.py
+++ multidc/addNode.py
@@ -3,17 +3,50 @@ import requests
 import json
 import time
 
+# TODO!
+# - get rid of the private key moron
+# - parse args
+# - addThing methods should return id value, or None with failure
+
+# Yay globals! These should get moved up/out to config file
+clustername = 'testcluster'
+opsc_url = '107.23.208.9:8888'
+datacenters = ['dc0','dc1','dc2']
+dcname = 'dc0'
+dcsize = 2
+
+dserepo = json.dumps({
+    "name":"DSE repo",
+    "username":"collin.poczatek+awstesting@gmail.com",
+    "password":"Cassandra1"})
+
+privkey = ""
+
+dsecred = json.dumps({
+    "become-mode":"sudo",
+    "use-ssh-keys":True,
+    "name":"dse creds",
+    "login-user":"ubuntu",
+    "ssh-private-key":privkey,
+    "become-user":None})
+
+defaultconfig = json.dumps({
+    "name":"Default config",
+    "datastax-version": "5.0.3",
+})
+
+# Constants that should go elsewhere?
+maxtrys = 100 #connection attempts
+timeout = 0.1 # connection timeout in sec
+pause = 6 # sleep between attempts in sec
+# maxtrys * pause = 600 sec or 10 min, should be enough time for
+# OpsC instance to come up.
+
+
 def pretty(data):
-    print json.dumps(data, sort_keys=True, indent=4)
+    print '\n', json.dumps(data, sort_keys=True, indent=4), '\n'
 
 def waitForOpsC(opsc_url):
-    # Constants that should go elsewhere?
-    maxtrys = 100 #connection attempts
-    timeout = 0.1 # connection timeout in sec
-    pause = 6 # sleep between attempts in sec
-    # maxtrys * pause = 600 sec or 10 min, should be enough time for
-    # OpsC instance to come up.
-
     count = 0
     while(True):
         count += 1
@@ -37,3 +70,204 @@ def waitForOpsC(opsc_url):
             data = meta.json()
             print("Found OpsCenter version: {version}".format(version=data['version']))
             return
+
+def checkForCluster(cname):
+    try:
+        clusters = requests.get("http://{url}/api/v1/lcm/clusters/".format(url=opsc_url)).json()
+        # This is a weak test. Assuming if there's any cluster,
+        # it's the one we want. Done in the name of expedience, to change.
+        if (clusters['count']==1):
+            return True
+        else:
+            return False
+    except requests.exceptions.Timeout as e:
+        print("Request for cluster config timed out.")
+        return False
+    except requests.exceptions.ConnectionError as e:
+        print("Request for cluster config refused.")
+        return False
+    except Exception as e:
+        # Do something?
+        raise
+    return (cname in clusterconf)
+
+def addCluster(cname, credid, repoid, configid):
+    try:
+        conf = json.dumps({
+            'name': cname,
+            'machine-credential-id': credid,
+            'repository-id': repoid,
+            'config-profile-id': configid})
+        clusterconf = requests.post("http://{url}/api/v1/lcm/clusters/".format(url=opsc_url),data=conf).json()
+        print("Added cluster, json:")
+        pretty(clusterconf)
+        return clusterconf['id']
+    except requests.exceptions.Timeout as e:
+        print("Request for cluster config timed out.")
+        return None
+    except requests.exceptions.ConnectionError as e:
+        print("Request for cluster config refused.")
+        return None
+    except Exception as e:
+        # Do something?
+        raise
+    return clusterconf
+
+def addCred(cred):
+    try:
+        creds = requests.get("http://{url}/api/v1/lcm/machine_credentials/".format(url=opsc_url)).json()
+        if (creds['count']==0):
+            creds = requests.post("http://{url}/api/v1/lcm/machine_credentials/".format(url=opsc_url),data=cred).json()
+            print("Added default dse creds, json:")
+            pretty(creds)
+            return creds
+    except requests.exceptions.Timeout as e:
+        print("Request to add ssh creds timed out.")
+        return None
+    except requests.exceptions.ConnectionError as e:
+        print("Request to add ssh creds refused.")
+        return None
+    except Exception as e:
+        # Do something?
+        raise
+
+def addConfig(conf):
+    try:
+        configs = requests.get("http://{url}/api/v1/lcm/config_profiles/".format(url=opsc_url)).json()
+        if (configs['count']==0):
+            config = requests.post("http://{url}/api/v1/lcm/config_profiles/".format(url=opsc_url),data=conf).json()
+            print("Added default condig profile, json:")
+            pretty(config)
+            return config
+    except requests.exceptions.Timeout as e:
+        print("Request to add config profile timed out.")
+        return None
+    except requests.exceptions.ConnectionError as e:
+        print("Request to add config profile refused.")
+        return None
+    except Exception as e:
+        # Do something?
+        raise
+
+def addRepo(repo):
+    try:
+        repos = requests.get("http://{url}/api/v1/lcm/repositories/".format(url=opsc_url)).json()
+        if (repos['count']==0):
+            repconf = requests.post("http://{url}/api/v1/lcm/repositories/".format(url=opsc_url),data=dserepo).json()
+            print("Added default repo, json:")
+            pretty(repconf)
+            return repconf
+    except requests.exceptions.Timeout as e:
+        print("Request to add repo timed out.")
+        return None
+    except requests.exceptions.ConnectionError as e:
+        print("Request to add repo refused.")
+        return None
+    except Exception as e:
+        # Do something?
+        raise
+
+def checkForDC(dcname):
+    try:
+        dcs = requests.get("http://{url}/api/v1/lcm/datacenters/".format(url=opsc_url)).json()
+        exists = False
+        for dc in dcs['results']:
+            if (dc['name'] == dcname):
+                exists = True
+        return exists
+    except requests.exceptions.Timeout as e:
+        print("Request to add repo timed out.")
+        return None
+    except requests.exceptions.ConnectionError as e:
+        print("Request to add repo refused.")
+        return None
+    except Exception as e:
+        # Do something?
+        raise
+
+def addDC(dcname, cid):
+    try:
+        dc = json.dumps({
+            'name': dcname,
+            'cluster-id': cid})
+        dcconf = requests.post("http://{url}/api/v1/lcm/datacenters/".format(url=opsc_url),data=dc).json()
+        print("Added datacenter {n}, json:".format(n=dcname))
+        pretty(dcconf)
+        return dcconf['id']
+    except requests.exceptions.Timeout as e:
+        print("Request to add repo timed out.")
+        return None
+    except requests.exceptions.ConnectionError as e:
+        print("Request to add repo refused.")
+        return None
+    except Exception as e:
+        # Do something?
+        raise
+
+def triggerInstall(dcid):
+    data = json.dumps({
+            "job-type":"install",
+            "job-scope":"datacenter",
+            "resource-id":dcid,
+            "auto-bootstrap":True,
+            "continue-on-error":False})
+    response = requests.post("http://{url}/api/v1/lcm/actions/install".format(url=opsc_url),data=data).json()
+    pretty(response)
+
+def main():
+    # eventual arg parsing
+    #parser = setupArgs()
+    #args = parser.parse_args()
+    # Should be args
+
+    waitForOpsC(opsc_url)  # Block waiting for OpsC to spin up
+
+    # return config instead of bool?
+    c = checkForCluster(clustername)
+    if (c == False): # cluster doesn't esist -> must be 1st node -> do setup
+        print("Cluster {n} doesn't exist, creating...".format(n=clustername))
+        cred = addCred(dsecred)
+        repo = addRepo(dserepo)
+        conf = addConfig(defaultconfig)
+        cid = addCluster(clustername, cred['id'], repo['id'], conf['id'])
+    else:
+        print("Cluster {n} exists".format(n=clustername))
+
+    # Check if the DC --this-- node should belong to exists, if not create-stack
+    c = checkForDC(dcname)
+    if (c == False):
+        print("Datacenter {n} doesn't exist, creating...".format(n=dcname))
+        clusters = requests.get("http://{url}/api/v1/lcm/clusters/".format(url=opsc_url)).json()
+        addDC(dcname,cid)
+    else:
+        print("Datacenter {d} exists".format(d=dcname))
+
+    # kludge, assuming ony one cluster
+    dcid = ""
+    datacenters = requests.get("http://{url}/api/v1/lcm/datacenters/".format(url=opsc_url)).json()
+    for d in datacenters['results']:
+        if (d['name'] == dcname):
+            dcid = d['id']
+
+    # always add self to DC
+    nodes = requests.get("http://{url}/api/v1/lcm/datacenters/{dcid}/nodes/".format(url=opsc_url,dcid=dcid)).json()
+    nodecount = nodes['count']
+    nodename = 'node'+str(nodecount)
+    privateip = requests.get("http://169.254.169.254/latest/meta-data/local-ipv4").content
+    nodeconf = json.dumps({
+            'name': nodename,
+            "datacenter-id": dcid,
+            "ssh-management-address": privateip})
+    node = requests.post("http://{url}/api/v1/lcm/nodes/".format(url=opsc_url),data=nodeconf).json()
+    print("Added node '{n}', json:".format(n=nodename))
+    pretty(node)
+
+    nodes = requests.get("http://{url}/api/v1/lcm/datacenters/{dcid}/nodes/".format(url=opsc_url,dcid=dcid)).json()
+    nodecount = nodes['count']
+    if (nodecount == dcsize):
+        print("Last node added, triggering install job...")
+        triggerInstall(dcid)
+
+# ----------------------------
+if __name__ == "__main__":
+    main()
