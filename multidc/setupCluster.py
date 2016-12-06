a83af17ae8ac5aaefa0852fb9672d54818f214c8
Collin Poczatek <collin.poczatek@gmail.com>
2016-12-06 12:25:13 -0800

    shifting cluster config

--- multidc/setupCluster.py
+++ multidc/setupCluster.py
@@ -129,6 +129,26 @@ def waitForOpsC(opsc_url):
             print("Found OpsCenter version: {version}".format(version=data['version']))
             return
 
+def checkForCluster(cname):
+    try:
+        clusters = requests.get("http://{url}/api/v1/lcm/clusters/".format(url=opsc_url)).json()
+        # This is a weak test. Assuming if there's any cluster,
+        # it's the one we want. Done in the name of expedience, to change.
+        if (clusters['count']==1):
+            return True
+        else:
+            return False
+    except requests.exceptions.Timeout as e:
+        print("Request for cluster config timed out.")
+        return False
+    except requests.exceptions.ConnectionError as e:
+        print("Request for cluster config refused.")
+        return False
+    except Exception as e:
+        # Do something?
+        raise
+    return (cname in clusterconf)
+
 
 def pretty(data):
     print '\n', json.dumps(data, sort_keys=True, indent=4), '\n'
