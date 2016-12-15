dfb734854a3815e4bcf375b54083036c727b52b9
Collin Poczatek <collin.poczatek@gmail.com>
2016-12-15 10:53:26 -0800

    script to get lcm public key

---
+++ util/getpubkey.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+keyname=$(aws cloudformation describe-stacks --query 'Stacks[?StackName==`opscenter-stack`].Parameters[] | [?ParameterKey==`KeyName`].ParameterValue' --output text)
+key=$(readlink -f ~/.ssh/$keyname.pem)
+opscip=$(aws cloudformation describe-stacks --query 'Stacks[?StackName==`opscenter-stack`].Outputs[] | [?OutputKey==`OpsCenterPublicIP`].OutputValue' --output text)
+ssh -o "StrictHostKeyChecking no" -i $key ubuntu@$opscip 'cat ~/.ssh/lcm.pem.pub'
