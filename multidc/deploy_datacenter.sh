71f1bd42b1a446334fe71f0099768311de4ea0c7
Collin Poczatek <collin.poczatek@gmail.com>
2016-12-20 16:23:35 -0800

    fix osx readlink bug, add instance type to dc deploy script

--- multidc/deploy_datacenter.sh
+++ multidc/deploy_datacenter.sh
@@ -5,6 +5,7 @@
 dcname="dc-us-east"
 region="us-east-1"
 dcsize=5
+instance="t2.medium"
 
 # Comment out/uncomment vvv to use a different key from the opscenter stack,
 # necessary when provisioning in a different region.
@@ -12,6 +13,20 @@ dcsize=5
 key=$(aws cloudformation describe-stacks --query 'Stacks[?StackName==`opscenter-stack`].Parameters[] | [?ParameterKey==`KeyName`].ParameterValue' --output text)
 #key="dse-keypair-us-west-1"
 ####################################################
+
+osxpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Running on MacOS..."
+    operatingSystem="MacOS"
+    template=$(osxpath "./findos.sh")
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Running on Linux..."
+    operatingSystem="Linux"
+    template=$(readlink -e cfn-datacenter-lcm.json)
+fi
 
 opscip=$(aws cloudformation describe-stacks --query 'Stacks[?StackName==`opscenter-stack`].Outputs[] | [?OutputKey==`OpsCenterPublicIP`].OutputValue' --output text)
 cname=$(aws cloudformation describe-stacks --query 'Stacks[?StackName==`opscenter-stack`].Parameters[] | [?ParameterKey==`ClusterName`].ParameterValue' --output text)
@@ -22,18 +37,20 @@ echo "Using opscenter ip: $opscip"
 echo "Using cluster name: $cname"
 echo "Using public key: $pubkey"
 echo "Using stack name: $stack"
+echo "Using template: $template"
 echo ""
 
 aws cloudformation create-stack \
 --region $region \
 --stack-name $stack \
---template-body file://$(readlink -f cfn-datacenter-lcm.json) \
+--template-body file://$template \
 --parameters \
 ParameterKey=KeyName,ParameterValue="$key" \
 ParameterKey=PublicKey,ParameterValue="$pubkey" \
 ParameterKey=ClusterName,ParameterValue="$cname" \
 ParameterKey=OpsCenterPubIP,ParameterValue="$opscip" \
 ParameterKey=DataCenterName,ParameterValue="$dcname" \
-ParameterKey=DataCenterSize,ParameterValue=$dcsize
+ParameterKey=DataCenterSize,ParameterValue=$dcsize \
+ParameterKey=InstanceType,ParameterValue=$instance
 
 echo "$stack $region" >> teardown.txt
--- multidc/deploy_opscenter.sh
+++ multidc/deploy_opscenter.sh
@@ -6,10 +6,24 @@ keyname="dse-keypair-us-east-1"
 clustername="mycluster"
 ####################################################
 
+osxpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Running on MacOS..."
+    operatingSystem="MacOS"
+    template=$(osxpath "./findos.sh")
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Running on Linux..."
+    operatingSystem="Linux"
+    template=$(readlink -e cfn-opscenter.json)
+fi
+
 stack="opscenter-stack"
 aws cloudformation create-stack \
 --stack-name $stack \
---template-body file://$(readlink -f cfn-opscenter.json) \
+--template-body file://$template \
 --parameters \
 ParameterKey=KeyName,ParameterValue=$keyname \
 ParameterKey=ClusterName,ParameterValue=$clustername
